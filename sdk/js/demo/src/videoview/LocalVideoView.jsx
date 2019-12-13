54230c82b2209dde5781198ad32c747436459c62
18871002288 <18871002288@163.com>
2019-12-13 19:21:00 +0800

    Add mute/unmute local audio and video icon

--- sdk/js/demo/src/videoview/LocalVideoView.jsx
+++ sdk/js/demo/src/videoview/LocalVideoView.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { setTimeout } from "timers";
+import MicrophoneIcon from "mdi-react/MicrophoneIcon";
+import MicrophoneOffIcon from "mdi-react/MicrophoneOffIcon";
+import VideoIcon from "mdi-react/VideoIcon";
+import VideocamOffIcon from "mdi-react/VideocamOffIcon";
 
 class LocalVideoView extends React.Component {
   componentDidMount = () => {
@@ -9,6 +13,7 @@ class LocalVideoView extends React.Component {
 
   render = () => {
     const { id, label, audioMuted, videoMuted } = this.props;
+    let allHide = !audioMuted && !videoMuted;
     return (
       <div className="local-video-layout">
         <div className="local-video-container">
@@ -22,9 +27,17 @@ class LocalVideoView extends React.Component {
             muted={true}
             className="local-video-size"
           />
+          <div className="local-video-icon-layout">
+            {
+              allHide ? "" : (audioMuted ? <MicrophoneOffIcon size={18} color="white"/> : <MicrophoneIcon size={18} color="white"/> )
+            }
+            {
+              allHide ? "" : (videoMuted ? <VideocamOffIcon size={18} color="white"/> : <VideoIcon size={18} color="white"/>)
+            }
+            
+          </div>
           <a className="local-video-name">
-            {label} audio: {audioMuted ? "off" : "on"} video:{" "}
-            {videoMuted ? "off" : "on"}
+            {label}
           </a>
         </div>
       </div>
