02f5c90ca919249e34abc4b799509590bbf4e1e8
CloudWebRTC <duanweiwei1982@gmail.com>
2019-12-18 23:06:13 +0800

    update.

--- sdk/flutter/lib/src/stream.dart
+++ sdk/flutter/lib/src/stream.dart
@@ -2,32 +2,20 @@ import 'package:events2/events2.dart';
 import 'package:flutter_webrtc/webrtc.dart';
 import 'logger.dart' show Logger;
 
-final Map<String, dynamic> mediaConstraints = {
-  "audio": true,
-  "video": {
-    "mandatory": {
-      "minWidth": '640',
-      "minHeight": '480',
-      "minFrameRate": '30',
-    },
-    "facingMode": "user",
-    "optional": [],
-  }
-};
-
 class Stream extends EventEmitter {
   var logger = new Logger("Ion::Stream");
   String _mid;
   MediaStream _stream;
-
   Stream([this._mid, this._stream]);
 
   init([sender = false, audio = true, video = true, screen = false]) async {
     if (sender) {
       if (screen) {
-        this._stream = await navigator.getDisplayMedia(mediaConstraints);
+        this._stream = await navigator
+            .getDisplayMedia(_buildMediaConstraints(false, true));
       } else {
-        this._stream = await navigator.getUserMedia(mediaConstraints);
+        this._stream =
+            await navigator.getUserMedia(_buildMediaConstraints(audio, video));
       }
     }
   }
@@ -39,4 +27,21 @@ class Stream extends EventEmitter {
   String get mid => _mid;
 
   MediaStream get stream => _stream;
+
+  _buildMediaConstraints(audio, video) {
+    return {
+      "audio": audio,
+      "video": video
+          ? {
+              "mandatory": {
+                "minWidth": '640',
+                "minHeight": '480',
+                "minFrameRate": '30',
+              },
+              "facingMode": "user",
+              "optional": [],
+            }
+          : false
+    };
+  }
 }
