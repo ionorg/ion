3ad32c4892ee7ed9d9c547e050a2804c148d1542
adwpc <adwpc@hotmail.com>
2020-03-28 12:44:00 +0800

    Doc and Scripts: fix scripts; add doc

--- scripts/installDeps.sh
+++ scripts/installDeps.sh
@@ -4,27 +4,27 @@ OS_TYPE=""
 . $APP_DIR/scripts/common
 
 
-# centos7
+# Ubuntu
 if [[ "$OS_TYPE" =~ "Ubuntu" ]];then
-    sudo apt-get install -y etcd redis-server rabbitmq-server nodejs-legacy npm
-    #TODO add nats install
+    sudo apt-get install -y etcd redis-server nodejs-legacy npm
+    wgetdl https://github.com/nats-io/nats-server/releases/download/v2.1.4/nats-server-v2.1.4-linux-amd64.zip
+    uz nats-server-v2.1.4-linux-amd64.zip
+    sudo cp nats-server-v2.1.4-linux-amd64/nats-server /usr/bin
     sudo npm install -g n
     sudo n stable 
 fi
 
+# Centos7
 if [[ "$OS_TYPE" =~ "CentOS" ]];then
     npm config set registry http://registry.cnpmjs.org/
     sudo yum install epel-release
-    sudo yum install -y etcd redis rabbitmq-server nodejs
+    sudo yum install -y etcd redis nodejs
     wgetdl https://github.com/nats-io/nats-server/releases/download/v2.1.4/nats-server-v2.1.4-amd64.rpm
     sudo rpm -ivh nats-server-v2.1.4-amd64.rpm
 fi
 
+# Mac
 if [[ "$OS_TYPE" =~ "Darwin" ]];then
-    brew install etcd redis rabbitmq nodejs nats-server
+    brew install etcd redis nodejs nats-server
 fi
 
-exit
-
-cd $APP_DIR/sdk/js
-npm i
