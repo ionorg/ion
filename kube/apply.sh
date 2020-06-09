77f4d43b543d228d85029254f8cf49b360b8147a
Leeward Bound <l@lwb.co>
2020-06-08 17:46:37 -0700

    updating jbrady patch

--- kube/apply.sh
+++ kube/apply.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-kubectl apply -f config.yaml
-kubectl apply -f biz.yaml
-kubectl apply -f islb.yaml
-kubectl apply -f sfu.yaml
-kubectl apply -f web.yaml
+set -e
 
-kubectl apply -f ingress.yaml
+test -z "$1" && NS="ion" || NS="$1"
+
+echo "Installing ion services into namespace $NS..."
+
+kubectl -n $NS apply -f config.yaml
+kubectl -n $NS apply -f biz.yaml
+kubectl -n $NS apply -f islb.yaml
+kubectl -n $NS apply -f sfu.yaml
+kubectl -n $NS apply -f web.yaml
+
+kubectl -n $NS apply -f ingress.yaml
--- kube/deps.sh
+++ kube/deps.sh
@@ -5,19 +5,21 @@ test -z "$1" && NS="ion" || NS="$1"
 
 echo "Installing data services into namespace $NS..."
 
+kubectl create namespace $NS || true
+
 ### Data Services
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-helm install -n $NS ion-nats bitnami/nats --namespace=ion \
+helm install ion-nats bitnami/nats --namespace=$NS \
   --set auth.enabled=false \
   --set metrics.enabled=true | tee -a helm.log
 
-helm install -n $NS ion-redis bitnami/redis --namespace=ion \
+helm install ion-redis bitnami/redis --namespace=$NS \
   --set usePassword=false \
   --set metrics.enabled=true | tee -a helm.log
 
-helm install -n $NS ion-etcd bitnami/etcd --namespace=ion \
+helm install ion-etcd bitnami/etcd --namespace=$NS \
   --set auth.rbac.enabled=false \
   --set metrics.enabled=true | tee -a helm.log
 
