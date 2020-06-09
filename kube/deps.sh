f8c4bc343a28ec885a21504433eae3cd72f44d2e
Leeward Bound <l@lwb.co>
2020-06-08 17:22:35 -0700

    updating deps to install in the proper namespace

--- kube/deps.sh
+++ kube/deps.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
+test -z "$1" && NS="ion" || NS="$1"
+
+echo "Installing data services into namespace $1..."
+
 ### Data Services
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
 
-helm install ion-nats bitnami/nats --namespace=ion \
+helm install -n $NS ion-nats bitnami/nats --namespace=ion \
   --set auth.enabled=false \
   --set metrics.enabled=true | tee -a helm.log
 
-helm install ion-redis bitnami/redis --namespace=ion \
+helm install -n $NS ion-redis bitnami/redis --namespace=ion \
   --set usePassword=false \
   --set metrics.enabled=true | tee -a helm.log
 
-helm install ion-etcd bitnami/etcd --namespace=ion \
+helm install -n $NS ion-etcd bitnami/etcd --namespace=ion \
   --set auth.rbac.enabled=false \
   --set metrics.enabled=true | tee -a helm.log
 
